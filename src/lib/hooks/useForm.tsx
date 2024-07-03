2422f5270dcdffa34a0667c193226d9f78c893df
Marco Raffaello <marco.raffaello@edalab.it>
2024-07-03 14:55:41 +0200

    Added form hooks memoization
    useForm and useFormFields hooks will now avoid every re-render after initialization.
    This will prevent form state reset.

--- src/lib/hooks/useForm.tsx
+++ src/lib/hooks/useForm.tsx
@@ -1,31 +1,34 @@
 import { ReInput, ReInputProps } from '../components';
 import { ZodType, z } from 'zod';
 import { ReForm, ReButton, ReSubscribe, ReFormProps, ZodDefinition } from '../components';
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { DeepPartial, ExtractFieldType, NestedKeyOf } from '../types/structs';
 
 // Todo: memoize result
 export function useForm<T extends ZodType<any, any, any>>(validator: T, initialValue?: z.infer<T>) {
     type InferredType = z.infer<T>;
 
-    return {
-        context: (props: Omit<ReFormProps<InferredType>, 'validator' | 'initialValue'>) => (
-            <ReForm
-                {...props}
-                validator={validator as unknown as ZodDefinition}
-                initialValue={initialValue}
-            />
-        ),
-        field: <Property extends NestedKeyOf<InferredType>>(
-            props: ReInputProps<Property, ExtractFieldType<InferredType, Property>>
-        ) => <ReInput {...props} />,
-        safeField: <Property extends NestedKeyOf<InferredType>>(
-            props: ReInputProps<Property, NonNullable<ExtractFieldType<InferredType, Property>>>
-        ) => <ReInput {...props} />,
-        subscribe: <R,>(props: {
-            selector: (value: DeepPartial<InferredType> | undefined) => R;
-            children: (subscribedValue: R) => ReactNode;
-        }) => <ReSubscribe<InferredType, R> {...props} />,
-        button: ReButton
-    };
+    return useMemo(
+        () => ({
+            context: (props: Omit<ReFormProps<InferredType>, 'validator' | 'initialValue'>) => (
+                <ReForm
+                    {...props}
+                    validator={validator as unknown as ZodDefinition}
+                    initialValue={initialValue}
+                />
+            ),
+            field: <Property extends NestedKeyOf<InferredType>>(
+                props: ReInputProps<Property, ExtractFieldType<InferredType, Property>>
+            ) => <ReInput {...props} />,
+            safeField: <Property extends NestedKeyOf<InferredType>>(
+                props: ReInputProps<Property, NonNullable<ExtractFieldType<InferredType, Property>>>
+            ) => <ReInput {...props} />,
+            subscribe: <R,>(props: {
+                selector: (value: DeepPartial<InferredType> | undefined) => R;
+                children: (subscribedValue: R) => ReactNode;
+            }) => <ReSubscribe<InferredType, R> {...props} />,
+            button: ReButton
+        }),
+        []
+    );
 }
--- src/lib/hooks/useFormField.tsx
+++ src/lib/hooks/useFormField.tsx
@@ -1,15 +1,20 @@
 import { ReInput, ReInputProps } from '../components';
 import { ZodType, z } from 'zod';
 import { ExtractFieldType, NestedKeyOf } from '../types/structs';
+import { useMemo } from 'react';
 
 export function useFormField<T extends ZodType<any, any, any>>() {
     type InferedType = z.infer<T>;
-    return {
-        field: <Property extends NestedKeyOf<InferedType>>(
-            props: ReInputProps<Property, ExtractFieldType<InferedType, Property>>
-        ) => <ReInput {...props} />,
-        safeField: <Property extends NestedKeyOf<InferedType>>(
-            props: ReInputProps<Property, NonNullable<ExtractFieldType<InferedType, Property>>>
-        ) => <ReInput {...props} />
-    };
+
+    return useMemo(
+        () => ({
+            field: <Property extends NestedKeyOf<InferedType>>(
+                props: ReInputProps<Property, ExtractFieldType<InferedType, Property>>
+            ) => <ReInput {...props} />,
+            safeField: <Property extends NestedKeyOf<InferedType>>(
+                props: ReInputProps<Property, NonNullable<ExtractFieldType<InferedType, Property>>>
+            ) => <ReInput {...props} />
+        }),
+        []
+    );
 }
